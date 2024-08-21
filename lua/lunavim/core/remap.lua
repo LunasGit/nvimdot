30b62cf266d5b4f61d504fc9ce2976dffa453b23
Luna <lunascodemail@gmail.com>
2024-08-21 05:53:18 -0400

    More bulk

--- lua/lunavim/core/remap.lua
+++ lua/lunavim/core/remap.lua
@@ -1,62 +1,67 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set("i", "jj", "<Esc>")
+local keyset = vim.keymap.set
+keyset("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+keyset("i", "jj", "<Esc>")
 
-vim.keymap.set("n", "H", "^")
-vim.keymap.set("n", "L", "$")
-vim.keymap.set("n", "M", "%")
+-- line nav qol
+keyset("n", "H", "^")
+keyset("n", "L", "$")
+keyset("n", "M", "%")
 
 -- colorpicker
-vim.keymap.set("n", "<C-c>", "<cmd>PickColor<cr>")
-vim.keymap.set("i", "<C-c>", "<cmd>PickColorInsert<cr>")
+keyset("n", "<C-c>", "<cmd>PickColor<cr>")
+keyset("i", "<C-c>", "<cmd>PickColorInsert<cr>")
 
 -- Lazy
-vim.keymap.set("n", "<leader>ll", vim.cmd.Lazy, { desc = "Open Lazy Window" })
+keyset("n", "<leader>ll", vim.cmd.Lazy, { desc = "Open Lazy Window" })
 
 -- telescope
-vim.keymap.set("n", '<leader>pf', '<cmd>Telescope find_files<CR>')
-vim.keymap.set("n", '<leader>b', '<cmd>Telescope buffers<CR>')
-vim.keymap.set("n", '<leader>ps', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set("n", '<C-p>', '<cmd>Telescope git_files<CR>')
+keyset("n", '<leader>pf', '<cmd>Telescope find_files<CR>')
+keyset("n", '<leader>b', '<cmd>Telescope buffers<CR>')
+keyset("n", '<leader>ps', '<cmd>Telescope live_grep<CR>')
+keyset("n", '<C-p>', '<cmd>Telescope git_files<CR>')
 
 -- pane navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+keyset("n", "<C-h>", "<C-w>h")
+keyset("n", "<C-j>", "<C-w>j")
+keyset("n", "<C-k>", "<C-w>k")
+keyset("n", "<C-l>", "<C-w>l")
 
-vim.keymap.set("n", "<leader>ts", "<C-w><C-s><C-w><C-j>:term<CR>") -- split window horizontally as terminal and jump to it
-vim.keymap.set("n", "<leader>tv", "<C-w>v<C-w><C-l>:term<CR>") -- split window vertically as terminal and jump to it
+keyset("n", "<leader>ts", "<C-w><C-s><C-w><C-j>:term<CR>") -- split window horizontally as terminal and jump to it
+keyset("n", "<leader>tv", "<C-w>v<C-w><C-l>:term<CR>") -- split window vertically as terminal and jump to it
 
 -- exit terminal mode with esc
-vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
+keyset("t", "<esc>", "<C-\\><C-n>")
 
 -- buffer
-vim.keymap.set("n", "<Tab>", vim.cmd.bnext) -- next buffer
-vim.keymap.set("n", "<leader>db", vim.cmd.bd) -- remove current buffer
+keyset("n", "<Tab>", vim.cmd.bnext) -- next buffer
+keyset("n", "<leader>db", vim.cmd.bd) -- remove current buffer
 
 -- general quality of life
-vim.keymap.set("v", "K",":m '<-2<CR>gv=gv") -- move selection up
-vim.keymap.set("v", "J",":m '>+1<CR>gv=gv") -- move selection down
+keyset("v", "K",":m '<-2<CR>gv=gv") -- move selection up
+keyset("v", "J",":m '>+1<CR>gv=gv") -- move selection down
 
-vim.keymap.set("n", "J", "mzJ`z") -- leave cursor in place while appending lines below
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- keep cursor in middle during half page jumping
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- keep cursor in middle during half page jumping
-vim.keymap.set("n", "n", "nzzzv") -- keep cursor in middle during searches
-vim.keymap.set("n", "N", "Nzzzv") -- keep cursor in middle during searches
+keyset("n", "J", "mzJ`z") -- leave cursor in place while appending lines below
+keyset("n", "<C-d>", "<C-d>zz") -- keep cursor in middle during half page jumping
+keyset("n", "<C-u>", "<C-u>zz") -- keep cursor in middle during half page jumping
+keyset("n", "n", "nzzzv") -- keep cursor in middle during searches
+keyset("n", "N", "Nzzzv") -- keep cursor in middle during searches
 
-vim.keymap.set("n", "<leader>y", "\"+y") -- yank into system clipboard
-vim.keymap.set("v", "<leader>y", "\"+y") -- yank into system clipboard visual mode
-vim.keymap.set("n", "<leader>Y", "\"+Y") -- yank full line to system clipboard
+keyset("n", "<leader>y", "\"+y") -- yank into system clipboard
+keyset("v", "<leader>y", "\"+y") -- yank into system clipboard visual mode
+keyset("n", "<leader>Y", "\"+Y") -- yank full line to system clipboard
 
-vim.keymap.set("n", "x", "\"_x") --single character delete into void
+keyset("n", "x", "\"_x") --single character delete into void
 
-vim.keymap.set("n", "<leader>lg", vim.cmd.LazyGit)
+keyset("n", "<leader>lg", vim.cmd.LazyGit)
 
 -- Fugitive Mapping
-vim.keymap.set("n", "<leader>gc", ':Git commit -m ""<left>')
-vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
-vim.keymap.set("n", "<leader>gd", ":Git diff<CR>")
+keyset("n", "<leader>gc", ':Git commit -m ""<left>')
+keyset("n", "<leader>gp", ":Git push<CR>")
+keyset("n", "<leader>gd", ":Git diff<CR>")
+
+-- replace all instances of word you are on
+keyset("n", "<leader>rn", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
 -- quickfix nav
 --[[ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -64,5 +69,3 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz") ]]
 
--- replace all instances of word you are on
-vim.keymap.set("n", "<leader>rn", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
