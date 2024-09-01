20fe958d8012574a99d8573a053f17400dfbc290
LunasGit <lunascodemail@gmail.com>
2024-08-31 20:55:26 -0400

    Change colorscheme
    removed gruvbox, switched to tokyonight

--- lua/lunavim/plugins/layout/colorscheme.lua
+++ lua/lunavim/plugins/layout/colorscheme.lua
@@ -1,11 +1,12 @@
 return{
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000 ,
-    config = true,
-    opts = ...},
-  {
     'rebelot/kanagawa.nvim' ,
     priority = 1000,
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
 }
