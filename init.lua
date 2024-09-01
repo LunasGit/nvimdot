74c28b744830eed7dceca099f52aee352a39334f
LunasGit <lunascodemail@gmail.com>
2024-08-31 21:01:27 -0400

    initial commit

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 require('lunavim/core/remap')
 require('lunavim/core/set')
 require('lunavim.lazy')
-vim.cmd[[colorscheme gruvbox]]
+vim.cmd[[colorscheme tokyonight-moon]]
+
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#a6fefb", bold = true })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ff96ff", bold = true })
